6585e5cf285bac557dd00c7c7c3053109191c3a5
Jonathan Delgado <hi@jon.soy>
2025-03-22 16:11:34 -0300

    feat(job-offers): Add job offer module and controller
    Adds a new job offer module and controller to the application. The job
    offer module provides endpoints for creating, listing, describing, and
    deleting job offers. This feature is necessary to allow users to
    manage job offers on the platform.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { JobOfferModule } from './job-offers/job-offert.module';
 
-@Module({})
+@Module({
+    imports: [JobOfferModule]
+})
 export class AppModule {}
---
+++ src/job-offers/job-offert.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { JobOfferController } from './job-offert.controller';
+
+@Module({
+    controllers: [JobOfferController],
+})
+export class JobOfferModule {}
