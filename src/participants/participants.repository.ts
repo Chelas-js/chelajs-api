8c678d7706b9803bcfc1bae8df7ef04f8a3b7920
Jonathan Delgado <hi@jon.soy>
2025-03-24 18:53:34 -0300

    feat(participants): implement CRUD operations for participants
    This commit introduces the implementation of CRUD (Create, Read, Update, Delete)
    operations for participants in the ParticipantsRepository. The changes include:
    
    - Injecting the ConfigService to retrieve the database ID from the configuration
    - Initializing the Firestore client and the participants collection reference
    - Implementing methods to set, get, retrieve, and delete participant data
    - Returning the appropriate data structures for the various operations
    
    These changes enable the application to manage participant data in the Firestore
    database, which is a key requirement for the application's functionality.

--- src/participants/participants.repository.ts
+++ src/participants/participants.repository.ts
@@ -1,4 +1,50 @@
+import { Firestore } from "@google-cloud/firestore";
 import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { ConfigurationDTO } from "../config/dtos/configuration.dto";
+import { ParticipantDTO } from "./dtos/participant.dto";
 
 @Injectable({})
-export class ParticipantsRepository {}
+export class ParticipantsRepository {
+  firestore: Firestore;
+  participantsCollections: FirebaseFirestore.CollectionReference<
+    FirebaseFirestore.DocumentData,
+    FirebaseFirestore.DocumentData
+  >;
+
+  constructor(configService: ConfigService<ConfigurationDTO>) {
+    this.firestore = new Firestore({
+      databaseId: configService.get("databaseId"),
+    });
+    this.participantsCollections = this.firestore.collection("job_offers");
+  }
+
+  async setParticipant(participant: ParticipantDTO) {
+    await this.participantsCollections.doc(participant.uid).set(participant);
+    return participant;
+  }
+
+  async getParticipant(uid: string) {
+    const doc = await this.participantsCollections.doc(uid).get();
+    if (!doc.exists) {
+      return null;
+    }
+    return doc.data();
+  }
+
+  async getParticipants() {
+    const docs = await this.participantsCollections
+      .orderBy("createdAt")
+      .limit(30)
+      .get();
+
+    return {
+      data: docs.docs.map((doc) => doc.data() as ParticipantDTO),
+    };
+  }
+
+  async deleteParticipant(uid: string) {
+    await this.participantsCollections.doc(uid).delete();
+    return true;
+  }
+}
