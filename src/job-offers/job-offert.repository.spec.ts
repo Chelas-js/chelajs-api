64ed1e87c9df95aabee2d3a0b1ea752b9863bb61
Jonathan Delgado <hi@jon.soy>
2025-03-24 18:29:15 -0300

    feat(job-offers): Implement job offer repository
    Adds a new job offer repository that interacts with Firestore to
    manage job offers. The repository provides methods to create, list,
    describe, and delete job offers.

---
+++ src/job-offers/job-offert.repository.spec.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect, spyOn } from "bun:test";
+import { JobOfferController } from "./job-offert.controller";
+import request from "supertest";
+import { Test } from "@nestjs/testing";
+import { Body, INestApplication } from "@nestjs/common";
+
+describe("JobOfferService", () => {});
