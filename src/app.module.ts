101eaaaeee407428c3d7b6041ef421835aff8ffb
Jonathan Delgado <hi@jon.soy>
2025-03-24 18:30:23 -0300

    feat(app): Add new modules and configure app
    This commit adds the `JobOfferModule`, `AuthModule`, and `ParticipantsModule` to the
    `AppModule`. It also configures the `ConfigModule` to load the application
    configuration and make it globally available. These changes are necessary to
    enable the core functionality of the application, including job offer
    management, authentication, and participant management.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,19 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from "@nestjs/common";
+import { JobOfferModule } from "./job-offers/job-offert.module";
+import { ConfigModule } from "@nestjs/config";
+import { configuration } from "./config/configuration";
+import { AuthModule } from "./auth/auth.module";
+import { ParticipantsModule } from "./participants/participants.module";
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    AuthModule.forGlobal(),
+    JobOfferModule,
+    ParticipantsModule,
+  ],
+})
 export class AppModule {}
