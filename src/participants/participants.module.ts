b7851cfa097ccc68226edfa46a62b2919231a909
Jonathan Delgado <hi@jon.soy>
2025-03-24 22:53:16 -0300

    feat(participants): add ParticipantsController to module
    Adds the ParticipantsController to the ParticipantsModule. This
    allows the ParticipantsController to handle HTTP requests and
    interact with the ParticipantsService and ParticipantsRepository.
    The changes enable the implementation of the participants
    functionality in the application.

--- src/participants/participants.module.ts
+++ src/participants/participants.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { ParticipantsService } from "./participants.service";
 import { ParticipantsRepository } from "./participants.repository";
+import { ParticipantsController } from "./participants.controller";
 
 @Module({
+  controllers: [ParticipantsController],
   providers: [ParticipantsRepository, ParticipantsService],
 })
 export class ParticipantsModule {}
